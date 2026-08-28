7d87baf8b17f9f99d878aef9cbbcbd0935a1777c
shivam-a-diggibyte <shivam.a@diggibyte.com>
2026-08-28 19:31:11 +0530

    Delete unnecesaary directory

--- src/Python_Practice-Shivam/Conditional_Statements/if_else.py
+++
@@ -1,6 +0,0 @@
-number = 7
-
-if number % 2 == 0:
-    print("Even number")
-else:
-    print("Odd number")
--- src/Python_Practice-Shivam/Python_Basics/python_syntax.py
+++
@@ -1,2 +0,0 @@
-# this is a way to write a comment in python
-print("Hello, World!") # it will print text on screen
