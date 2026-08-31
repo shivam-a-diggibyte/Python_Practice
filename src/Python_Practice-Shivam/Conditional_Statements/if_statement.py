ab22469f5e3376e171f81a71df78d58d6c2eb733
Shivam Kumar <shivam.a@diggibyte.com>
2026-08-31 10:55:49 +0530

    Add Python conditional statements practice

---
+++ src/Python_Practice-Shivam/Conditional_Statements/if_statement.py
@@ -0,0 +1,14 @@
+age = 20
+
+if age >= 18:
+    print("Eligible to vote")
+
+
+marks = 72
+
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 60:
+    print("Grade: B")
+else:
+    print("Grade: C")
