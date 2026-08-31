b4b8947076e9d4467f7ce90ef2ba7ec802ab070a
Shivam Kumar <shivam.a@diggibyte.com>
2026-08-31 11:07:07 +0530

    Add Python conditional statements practice

---
+++ src/Conditional_Statements/if_elif_else.py
@@ -0,0 +1,10 @@
+marks = 85
+
+if marks >= 90:
+    print("Grade A+")
+elif marks >= 80:
+    print("Grade A")
+elif marks >= 70:
+    print("Grade B")
+else:
+    print("Grade C")
---
+++ src/Conditional_Statements/if_else.py
@@ -0,0 +1,6 @@
+number = 7
+
+if number % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
