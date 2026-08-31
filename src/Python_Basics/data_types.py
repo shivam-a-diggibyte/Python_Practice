141c21fc582ebb191afd3557aaa96f4732f5a811
Shivam Kumar <shivam.a@diggibyte.com>
2026-08-31 18:14:34 +0530

    Update Python data types practice

--- src/Python_Basics/data_types.py
+++ src/Python_Basics/data_types.py
@@ -2,3 +2,4 @@ print(type(10))        # <class 'int'>
 print(type(3.14))      # <class 'float'>
 print(type("hello"))   # <class 'str'>
 print(type(True))      # <class 'bool'>
+print(type(76j))       # <class 'complex'>
