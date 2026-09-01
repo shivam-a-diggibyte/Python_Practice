ee81427837f66c5a49a3c6c5253f08ee607640fd
Shivam Kumar <shivam.a@diggibyte.com>
2026-09-01 17:51:27 +0530

    Update function return value practice

--- src/Functions/Return_Value.py
+++ src/Functions/Return_Value.py
@@ -1,5 +1,18 @@
-def add(a, b):
+def add(a, b): #single return value
     return a + b
 
 result = add(5, 7)
 print(result)     # 12
+
+
+def calculate(a, b): #multiple return values
+    addition = a + b
+    multiplication = a * b
+
+    return addition, multiplication
+
+
+add_result, multiply_result = calculate(10, 5)
+
+print("Addition:", add_result)
+print("Multiplication:", multiply_result)
