ba75291268a1e6de53b88cf3039c7626c2f6ea2b
Shivam Kumar <shivam.a@diggibyte.com>
2026-09-02 15:26:35 +0530

    Add main Python practice script

---
+++ src/main.py
@@ -0,0 +1,5 @@
+from Utilities_Package.calculator import add
+from Utilities_Package.string_util import reverse_text
+
+print("Addition of 5 and 3 is :",str(add(5,3)))
+print("Reversed text is :", reverse_text("Python"))
